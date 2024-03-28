7046f2c66c0301b84d3d7cfd0ae4b382b2aa5935
Tanvir Ahmed Talukder <rahidt78@gmail.com>
2024-03-28 13:47:27 +0600

    Show name from input.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+import { useRef } from "react";
+
 export default function Player() {
+  const [enteredName, setEnteredName] = useState("");
+
+  const playerName = useRef();
+
+  function handleClick(e) {
+    setEnteredName(playerName.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredName ? enteredName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
